ec19cabfc196db0a9c865512ba3ce8f2d61e0105
ShubhamD1x1T <dixitshubham189@gmail.com>
2022-02-07 13:27:36 +0530

    ExecutionContext

---
+++ executionContext.js
@@ -0,0 +1,23 @@
+//console.log("hello");
+//console.log(global);
+//console.log(this);
+
+// var a = 20;
+
+// function fn(){
+//     var a = 30;
+//     console.log(a);
+// }
+// fn();
+
+// console.log(a);
+// var a;
+// console.log(a);
+// a = 10;
+// console.log(a);
+
+// fn();
+
+// function fn(){
+//     console.log("called before dec");
+// }
---
+++ var_let_const.js
@@ -0,0 +1,16 @@
+// let a 
+// a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+// let a = 30;
+// console.log(a);
+
+// console.log(a)  //can't access before initialization
+//let a;
+
+// const b ; // not allowed in const
+// b = 10  
+
+const b = 10;
+console.log(b);
